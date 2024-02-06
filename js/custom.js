f4f5cc29df8729d96c607a21b505873367e9a7a4
Emilia <emilia.suentassu@gmail.com>
2024-02-06 15:17:55 +0100

    making header. Mobile version is almost done.

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/* Main navigation (show/hide links in mobile/tablet)*/
+function naviLinks() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
