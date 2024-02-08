586a6359a95be160f53d256e0fa624fba99d33eb
Emilia <emilia.suentassu@gmail.com>
2024-02-08 13:28:12 +0100

    Footer boxes done. It is responsive, ACF is only thing that is missing

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 
         <div class="footer-container">
             <div class="footer-content">
-        Footer
+        Footer tekstiä niin näkee,että kuinka iso tästä laatikosta tulee ku on enemmän systeemejä tämän sisällä.
             </div>
             <div class="footer-content">
         Footer
