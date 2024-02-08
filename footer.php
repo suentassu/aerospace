ac61d898ab6bc41d1cc84d03c53b2ec4bee69fe2
Emilia <emilia.suentassu@gmail.com>
2024-02-08 11:43:45 +0100

    Footer update. Some changes to setup.css

--- footer.php
+++ footer.php
@@ -1,7 +1,21 @@
 </main>
-
+<div class="red-line"></div>
 <footer>
-    Footer
+    <div class="footer-overlay">
+
+        <div class="footer-container">
+            <div class="footer-content">
+        Footer
+            </div>
+            <div class="footer-content">
+        Footer
+            </div>
+
+
+        </div>
+
+
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
