2bd3859ab4cb06bb843c5492e978154d023e621e
Nella S <nellasoudunsaari@gmail.com>
2024-02-08 15:40:29 +0200

    More responsiveness + astronaut

--- courses-page.php
+++ courses-page.php
@@ -10,17 +10,17 @@ get_header();
     <div class="courses-section-inner">
         <h1 class="courses-title"> Courses </h1>
         <div class="floating-astronaut">
-
+            <img src="/img/astronaut.png" alt="astornaut">
         </div>
         <div class="courses-row">
             <div class="course" style="background-color: #aaa">
-                <p>yadaYADAYyafya</p>
+                <p>Course name</p>
             </div>
             <div class="course" style="background-color: #aaa">
-                <p>yadaYADAYyafya</p>
+                <p>Course name</p>
             </div>
             <div class="course" style="background-color: #aaa">
-                <p>yadaYADAYyafya</p>
+                <p>Course name</p>
             </div>
         </div>
     </div>
