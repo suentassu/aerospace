636892d753a0b89562d20e8b257d0fb0732ec985
Emilia <emilia.suentassu@gmail.com>
2024-02-08 14:51:19 +0100

    Merged and uptated about-page branch

--- about-page.php
+++ about-page.php
@@ -1,18 +1,21 @@
 <?php
 /**
- * About-page file.
+ * Template Name: About page
  * 
- * @package AeroSpace
  */
+
 get_header();
 ?>
 
-<main id="site-content">
-    <div class="section-inner">
-        <h1 class="title"> About-page </h1>
-    </div>
-</main>
+
+
+
 
 <?php
-get_footer();
+        the_content();
 ?>
+
+
+
+
+<?php get_footer(); ?>
