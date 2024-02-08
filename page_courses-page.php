39d313f9ecb144a80e719abbab55dcd55211157a
Nella S <nellasoudunsaari@gmail.com>
2024-02-08 10:04:05 +0200

    Modified the courses page filename to make it a template (courses-page.php -> page_courses-page.php), added .row and .column classes to setup.css and started modifying the actual webpage

---
+++ page_courses-page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<?php
+/**
+ * Template Name: Courses Page
+ * 
+ * @package AeroSpace
+ */
+get_header();
+?>
+<main id="site-content">
+    <div class="section-inner">
+        <h1 class="title"> Courses </h1>
+        <div class="floating-astronaut"></div>
+        <div class="row">
+            <div class="column" style="background-color: #aaa">
+                <p>yadaYADAYyafya</p>
+            </div>
+            <div class="column" style="background-color: #aaa">
+                <p>yadaYADAYyafya</p>
+            </div>
+            <div class="column" style="background-color: #aaa">
+                <p>yadaYADAYyafya</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php 
+get_footer();
+?>
+
