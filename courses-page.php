a2c200f223dfec1a5db52b5b4ae56ecddd5a1eff
Nella S <nellasoudunsaari@gmail.com>
2024-02-09 12:45:32 +0200

    Smoother animation for astronaut

--- courses-page.php
+++ courses-page.php
@@ -10,7 +10,7 @@ get_header();
     <div class="courses-section-inner">
         <h1 style="text-align: center;"> Courses </h1>
         <div class="astronaut-div">
-            <img class="floating-astronaut" src="../wp-content/themes/aerospace/img/astronaut.png" alt="astornaut">
+            <img class="floating-astronaut" src="../wp-content/themes/aerospace/img/astronaut-flipped.png" alt="astornaut">
         </div>
         <div class="courses-row">
             <div class="course" style="background-color: #aaa">
