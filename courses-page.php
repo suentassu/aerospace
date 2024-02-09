e3baf5cccdf64b9126f9234987bb5d410f4cf1b5
Nella S <nellasoudunsaari@gmail.com>
2024-02-09 16:36:31 +0200

    Finishing the courses page, there will be some modifications but the layout is pretty much done

--- courses-page.php
+++ courses-page.php
@@ -6,21 +6,29 @@
  */
 get_header();
 ?>
-
+    <div class="back-button">
+        <a href="here will be a link to an earlier page" title="here will be a link to an earlier page"> Here will be a link to an earlier page</a>
+    </div>
     <div class="courses-section-inner">
         <h1 style="text-align: center;"> Courses </h1>
         <div class="astronaut-div">
             <img class="floating-astronaut" src="../wp-content/themes/aerospace/img/astronaut-flipped.png" alt="astornaut">
         </div>
         <div class="courses-row">
-            <div class="course" style="background-color: #aaa">
-                <p>Course name</p>
+            <div class="course">
+                <div class="title-bar">
+                    <p class="course-title">Course name</p>
+                </div>  
             </div>
-            <div class="course" style="background-color: #aaa">
-                <p>Course name</p>
+            <div class="course">
+                <div class="title-bar">
+                    <p class="course-title">Course name</p>
+                </div>
             </div>
-            <div class="course" style="background-color: #aaa">
-                <p>Course name</p>
+            <div class="course">
+                <div class="title-bar">
+                    <p class="course-title">Course name</p>
+                </div>
             </div>
         </div>
     </div>
