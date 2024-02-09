14b0327510261a5745437e820af5a07bbf8f01bb
Emilia <emilia.suentassu@gmail.com>
2024-02-09 11:51:02 +0100

    Changes to about-page

--- about-page.php
+++ about-page.php
@@ -7,15 +7,15 @@
 get_header();
 ?>
 
+<div class="content-container">
 
-
-
+<h1 class="first-title"><?php echo get_the_title(); ?></h1>
 
 <?php
         the_content();
 ?>
 
 
-
+</div>
 
 <?php get_footer(); ?>
