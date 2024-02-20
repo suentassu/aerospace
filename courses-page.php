a7c325da094687372ba5dd6c5b44a014c027dcef
Nella S <nellasoudunsaari@gmail.com>
2024-02-20 12:46:45 +0200

    Testing stuff with courses

--- courses-page.php
+++ courses-page.php
@@ -17,9 +17,16 @@ get_header();
         <!-- EACH COURSE WILL HAVE THEIR OWN IMAGES -->
         <div class="courses-row">
                 <div class="course">
-                    <div class="title-bar">
-                        <p class="course-title">Course name</p> 
-                        <img src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                    <div class="course-overlay">
+                        <div class="course-content">
+                            <div class="title-bar">
+                                <p class="course-title">Course name</p>
+                            </div>
+                        </div>
+                        <div class="course-img">
+                            <img src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                        </div>
+                        
                     </div>
                 </div>
                 <div class="course">
@@ -33,6 +40,39 @@ get_header();
                         <p class="course-title">Course name</p> 
                         <img src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="courses-row">
+        <div class="testi-container">
+            <div class="testi-img">
+                <img src="../wp-content/themes/aerospace/img/stars-1.jpg"/>   
+            </div>
+            <div class="testi-overlay">
+                <div class="testi-title">
+                        <p class="testi-name">Course Name</p>
+                </div>
+            </div>
+        </div>
+        <div class="testi-container">
+            <div class="testi-img">
+                <img src="../wp-content/themes/aerospace/img/stars-1.jpg"/>   
+            </div>
+            <div class="testi-overlay">
+                <div class="testi-title">
+                        <p class="testi-name">Course Name</p>
+                </div>
+            </div>
+        </div>
+        <div class="testi-container">
+            <div class="testi-img">
+                <img src="../wp-content/themes/aerospace/img/stars-1.jpg"/>   
+            </div>
+            <div class="testi-overlay">
+                <div class="testi-title">
+                        <p class="testi-name">Course Name</p>
                 </div>
             </div>
         </div>
