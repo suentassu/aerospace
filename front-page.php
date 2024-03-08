f0eed97716e1bd368c1770c46483bd6cc3c64221
Daileyz <501936@vistacollege.nl>
2024-03-08 10:07:22 +0100

    Front page update
    added code to front-page

--- front-page.php
+++ front-page.php
@@ -6,13 +6,32 @@
 get_header();
 ?>
 
-<div class="content-container">
-        <h1 class="first-title"><?php echo get_the_title(); ?></h1>
-                <?php
-                        the_content();
-                ?>
+<div class="content-container front-page">
+    <h1 class="first-title"><?php echo get_the_title(); ?></h1>
+    <?php the_content(); ?>
 </div>
 
-<!-- Main meny pics -->
 
-<?php get_footer(); ?>
+
+<body>
+    <div class="container">
+        <div class="block">
+            <!-- Placeholder image in the first block -->
+            <img src="<?php echo get_template_directory_uri(); ?>/img/astronaut-11080_1280.jpg" alt="Image 1">
+            <!-- Text above the link in the first block -->
+            <p class="block-text">Heading for this item</p>
+            <!-- Link in the first block -->
+            <a href="#" class="block-link">Get Started</a>
+        </div>
+        <div class="block">
+            <!-- Placeholder image in the second block -->
+            <img src="<?php echo get_template_directory_uri(); ?>/img/space-station-60615_1280.jpg" alt="Image 2">
+            <!-- Text above the link in the second block -->
+            <p class="block-text">Heading for this item</p>
+            <!-- Link in the second block -->
+            <a href="#" class="block-link">Get Started</a>
+        </div>
+    </div>
+
+    <?php get_footer(); ?>
+
--- header.php
+++ header.php
@@ -1,48 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri()); ?>" type="text/css" />
+
+    <?php
+    // Enqueue front-page.css on the front page
+    if (is_front_page()) {
+        wp_enqueue_style('front-page-styles', get_template_directory_uri() . '/css/front-page.css', array(), '1.0', 'all');
+    }
+
+    // Additional head content
+    wp_head();
+    ?>
 </head>
 <body>
 
-    
 <header>
     <nav id="navbar">
-        
         <div class="nav-container">
             <div class="main-title">
-                <a href="<?php echo get_home_url(); ?>"><?php bloginfo(); ?></a>
+                <a href="<?php echo esc_url(get_home_url()); ?>"><?php bloginfo(); ?></a>
             </div>
             <div class="nav-icon">
                 <span class="icon-mobile" onclick="openNavi()"><i class="fa-solid fa-bars"></i></span>
-
                 <span class="icon-tablet" onclick="openNaviTablet()"><i class="fa-solid fa-bars"></i></span>
-                
                 <span class="icon-desk" onclick="openNaviDesk()"><i class="fa-solid fa-bars"></i></span>
             </div>
         </div>
 
         <div id="aeroNav" class="nav-overlay">
-                <a href="javascript:void(0)" class="nav-closebtn" onclick="closeNav()">
-                    <i class="fa-solid fa-x"></i>
-                </a>
-                
+            <a href="javascript:void(0)" class="nav-closebtn" onclick="closeNav()">
+                <i class="fa-solid fa-x"></i>
+            </a>
+
             <div class="nav-overlay-content">
-                <?php
-                    wp_nav_menu();
-                ?>
+                <?php wp_nav_menu(); ?>
             </div>
         </div>
-
     </nav>
 </header>
 
 <main class="main-overlay">
-
     <div class="back-button nav-container ">
         <a href="#" title="Back"><i class="fa-solid fa-circle-chevron-left"></i> Go back</a>
     </div>
