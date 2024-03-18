4109b71834f24da255556787ca801391ace702bb
Daileyz <501936@vistacollege.nl>
2024-03-18 15:20:14 +0100

    front-page
    made the class names a bit more specific

--- front-page.php
+++ front-page.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Template Name: Front page
- * 
  */
 get_header();
 ?>
@@ -11,23 +10,23 @@ get_header();
     <?php the_content(); ?>
 </div>
 
-<body>
-    <div class="container">
-        <div class="block">
-            <!-- Placeholder image in the first block -->
-            <img src="<?php echo get_template_directory_uri(); ?>/img/astronaut-11080_1280.jpg" alt="Image 1">
-            <!-- Text above the link in the first block -->
-            <p class="block-text">Heading for this item</p>
-            <!-- Link in the first block -->
-            <a href="#" class="block-link">Get Started</a>
-        </div>
-        <div class="block">
-            <!-- Placeholder image in the second block -->
-            <img src="<?php echo get_template_directory_uri(); ?>/img/space-station-60615_1280.jpg" alt="Image 2">
-            <!-- Text above the link in the second block -->
-            <p class="block-text">Heading for this item</p>
-            <!-- Link in the second block -->
-            <a href="#" class="block-link">Get Started</a>
-        </div>
+<div class="block-container">
+    <div class="image-block">
+        <!-- Placeholder image in the first block -->
+        <img src="<?php echo get_template_directory_uri(); ?>/img/astronaut-11080_1280.jpg" alt="Image 1">
+        <!-- Text above the link in the first block -->
+        <p class="block-text">Heading for this item</p>
+        <!-- Link in the first block -->
+        <a href="#" class="block-link">Get Started</a>
     </div>
-    <?php get_footer(); ?>
+    <div class="image-block">
+        <!-- Placeholder image in the second block -->
+        <img src="<?php echo get_template_directory_uri(); ?>/img/astronaut-11080_1280.jpg" alt="Image 2">
+        <!-- Text above the link in the second block -->
+        <p class="block-text">Heading for this item</p>
+        <!-- Link in the second block -->
+        <a href="#" class="block-link">Get Started</a>
+    </div>
+</div>
+
+<?php get_footer(); ?>
