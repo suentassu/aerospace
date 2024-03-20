fd8f827cf5f1dc75523127bba0e1ed34fb8453a7
youri boersma <507762@vistacollege.nl>
2024-03-20 09:22:20 +0100

    video page document and code

---
+++ video-page.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * Template Name: video page
+ *
+ */
+
+get_header();
+?>
+    <h1 class="first-title">video page</h1>
+    <div class="course-page-inner-section">
+        <div class="other-courses-title">Other Courses</div>
+        <br/>
+        <div class="course-content-area">
+            <div id="carousel">
+                <div class="swiffy-slider slider-item-show1 slider-item-ratio slider-item-ratio-4x3 slider-nav-round slider-nav-outside-expand slider-nav-visible slider-indicators-round slider-item-helper">
+                    <ul class="slider-container">
+                        <li>
+                            <div id="slide1">
+                                <div class="slider-course-thumbnail">
+                                    <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                </div>
+                                <div class="slider-course-info">
+                                    <p class="slider-course-title">Course Name 1</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="slide2">
+                                <div class="slider-course-thumbnail">
+                                    <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                </div>
+                                <div class="slider-course-info">
+                                    <p class="slider-course-title">Course Name 2</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="slide3">
+                                <div class="slider-course-thumbnail">
+                                    <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                </div>
+                                <div class="slider-course-info">
+                                    <p class="slider-course-title">Course Name 3</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="slide4">
+                                <div class="slider-course-thumbnail">
+                                    <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                </div>
+                                <div class="slider-course-info">
+                                    <p class="slider-course-title">Course Name 4</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="slide5">
+                                <div class="slider-course-thumbnail">
+                                    <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                </div>
+                                <div class="slider-course-info">
+                                    <p class="slider-course-title">Course Name 5</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="slide6">
+                                <div class="slider-course-thumbnail">
+                                    <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                </div>
+                                <div class="slider-course-info">
+                                    <p class="slider-course-title">Course Name 6</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+                    <span class="slider-nav"></span>
+                    <span class="slider-nav slider-nav-next"></span>
+                </div>
+            </div>
+        </div>
+
+        <br/><div class="other-courses-title">Other Courses</div><br/>
+
+            <div class="course-content-area">
+                <div id="carousel">
+                    <div class="swiffy-slider slider-item-show1 slider-item-ratio slider-item-ratio-4x3 slider-nav-round slider-nav-outside-expand slider-nav-visible slider-indicators-round slider-item-helper">
+                        <ul class="slider-container">
+                            <li>
+                                <div id="slide1">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 1</p>
+
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide2">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 2</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide3">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 3</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide4">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 4</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide5">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 5</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide6">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 6</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <span class="slider-nav"></span>
+                        <span class="slider-nav slider-nav-next"></span>
+                    </div>
+                </div>
+            </div>
+
+            <br/><div class="other-courses-title">Other Courses</div><br/>
+
+            <div class="course-content-area">
+                <div id="carousel">
+                    <div class="swiffy-slider slider-item-show1 slider-item-ratio slider-item-ratio-4x3 slider-nav-round slider-nav-outside-expand slider-nav-visible slider-indicators-round slider-item-helper">
+                        <ul class="slider-container">
+                            <li>
+                                <div id="slide1">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 1</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide2">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 2</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide3">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 3</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide4">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 4</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide5">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 5</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div id="slide6">
+                                    <div class="slider-course-thumbnail">
+                                        <img id="slider-course-thumbnail" src="../wp-content/themes/aerospace/img/stars-1.jpg"/>
+                                    </div>
+                                    <div class="slider-course-info">
+                                        <p class="slider-course-title">Course Name 6</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <span class="slider-nav"></span>
+                        <span class="slider-nav slider-nav-next"></span>
+                    </div>
+                </div>
+            </div>
+        <br/><br/>s
+<?php get_footer(); ?>
