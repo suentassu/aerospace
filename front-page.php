69cd4a15babe7c616de5cef71659054469f0bd53
Emilia <emilia.suentassu@gmail.com>
2024-03-22 13:13:09 +0100

    Bug fixes and cleaning some code

--- front-page.php
+++ front-page.php
@@ -20,18 +20,18 @@ get_header();
         <div class="overlay"></div> 
         <img src="<?php the_field('first_image'); ?>" alt="Image 1">
             <!-- Text above the link in the first block -->
-            <h2 class="block-text" style="color: <?php the_field('left_side_title_color_picker'); ?>;"><?php the_field('first_title'); ?></h2>
+            <h2 class="block-text centered" style="color: <?php the_field('left_side_title_color_picker'); ?>;"><?php the_field('first_title'); ?></h2>
             <!-- Link in the first block -->
-            <a href="<?php the_field('first_url'); ?>" class="block-link"><?php the_field('first_link_title'); ?></a>
+            <a href="<?php the_field('first_url'); ?>" class="block-link centered"><?php the_field('first_link_title'); ?></a>
     </div>
 
     <div class="image-block">
         <div class="overlay"></div>
         <img src="<?php the_field('second_image'); ?>" alt="Image 2">
         <!-- Text above the link in the second block -->
-        <h2 class="block-text" style="color: <?php the_field('right_side_title_color_picker'); ?>;"><?php the_field('second_title'); ?></h2>
+        <h2 class="block-text centered" style="color: <?php the_field('right_side_title_color_picker'); ?>;"><?php the_field('second_title'); ?></h2>
         <!-- Link in the second block -->
-        <a href="<?php the_field('second_url'); ?>" class="block-link"><?php the_field('second_link_title'); ?></a>
+        <a href="<?php the_field('second_url'); ?>" class="block-link centered"><?php the_field('second_link_title'); ?></a>
     </div>
 </div>
 
